1ac39b6a1c8c5a5d045d16ef087539956cdb26db
bradyrx <riley.brady@colorado.edu>
2017-09-16 14:05:40 -0600

    discrete colormap documentation

--- esmtools/visualization.py
+++ esmtools/visualization.py
@@ -8,7 +8,7 @@ Color
 import numpy as np
 import matplotlib.pyplot as plt
 
-def discrete_cmap(levels, base_cmap=None):
+def discrete_cmap(levels, base_cmap):
     """
     Returns a discretized colormap based on the specified input colormap.
 
@@ -21,7 +21,19 @@ def discrete_cmap(levels, base_cmap=None):
 
     Returns
     ------
-    discrete_cmap : 
+    discrete_cmap : LinearSegmentedColormap
+           Discretized colormap for plotting
+
+    Examples
+    --------
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import esmtools as et
+    data = np.random.randn(50,50)
+    plt.pcolor(data, vmin=-3, vmax=3, cmap=et.visualization.discrete_cmap(10,
+               "RdBu"))
+    plt.colorbar()
+    plt.show()
     """
     base = plt.cm.get_cmap(base_cmap)
     color_list = base(np.linspace(0, 1, levels))

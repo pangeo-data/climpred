31b54af1ce8d23319bdae4211cbad8d3d9317d24
bradyrx <riley.brady@colorado.edu>
2017-09-16 13:50:52 -0600

    discrete colorbar

--- esmtools/__init__.py
+++ esmtools/__init__.py
@@ -10,12 +10,12 @@ Available Modules:
 
 1. filtering: Contains definitions that assist in spatial and temporal
 filtering of output.
-2. mapping: Contains definitions for projecting global or regional data
-with cartopy.
+2. visualization: Contains definitions for colorbars, color maps, and any sort
+of global or regional projections.
 3. statistics: Contains definitions for computing general statistics on output.
 
 """
 
 from . import filtering
-from . import mapping
+from . import viusualization 
 from . import statistics
---
+++ esmtools/visualization.py
@@ -0,0 +1,30 @@
+"""
+Objects dealing with anything visualization.
+
+Color
+-----
+- `discrete_cmap` : Create a discrete colorbar for the visualization.
+"""
+
+import matplotlib.pyplot as plt
+
+def discrete_cmap(levels, base_cmap=None):
+    """
+    Returns a discretized colormap based on the specified input colormap.
+
+    Parameters
+    ----------
+    levels : int
+           Number of divisions for the color bar.
+    base_cmap : string
+           Colormap to discretize (can pull from cmocean, matplotlib, etc.)
+
+    Returns
+    ------
+    discrete_cmap : 
+    """
+    base = plt.cm.get_cmap(base_cmap)
+    color_list = base(np.linspace(0, 1, N))
+    cmap_name = base.name + str(N)
+    discrete_cmap = base.from_list(cmap_name, color_list, N)
+    return discrete_cmap

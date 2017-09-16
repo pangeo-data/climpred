fcad87b26308673c2c6fe5a615896b5f4919b6ae
bradyrx <riley.brady@colorado.edu>
2017-09-16 12:47:13 -0600

    added setup and stats

--- esmtools/__init__.py
+++ esmtools/__init__.py
@@ -1,3 +1,20 @@
 """
-Currently there is nothing in the init file.
+ESMtools
+-------
+
+A package for analyzing, processing, and mapping ESM output with an emphasis
+on ocean model output in particular.
+
+Available Modules:
+-----------------
+
+1. filtering: Contains definitions that assist in spatial and temporal
+filtering of output.
+2. mapping: Contains definitions for projecting global or regional data
+with cartopy.
+3. statistics: Contains definitions for computing general statistics on output.
+
 """
+
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='esmtools',
+      version='0.1',
+      description='Toolbox for analyzing ESM output, with an emphasis on ocean model output',
+      url='http://github.com/bradyrx/esmtools',
+      author='Riley X. Brady',
+      author_email='riley.brady@colorado.edu',
+      license='MIT',
+      packages=['esmtools'],
+      zip_safe=False)

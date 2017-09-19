4ce5f2f489556527c776e52f390688e037cd04d5
Riley X. Brady <riley.brady@colorado.edu>
2017-09-19 12:29:50 -0600

    ufuncs added
    Former-commit-id: d71798c2a63d79d4c3d78a8060b366fe027f1e2a

---
+++ esmtools/ufunc.py
@@ -0,0 +1,60 @@
+"""
+Objects optimized for using in .apply() functions in xarray datasets. 
+"""
+
+import numpy as np
+import numpy.polynomial.polynomial as poly
+
+def remove_polynomial_fit(ds):
+    """
+    Removes a 4th order polynomial fit from an xarray dataset.
+    Returns the detrended timeseries.
+
+    Parameters
+    ----------
+    ds : xarray Dataset
+
+    Returns
+    -------
+    detrended : xarray DataArray
+
+    Examples
+    --------
+    """
+    # Deals with cases where it is a NaN time series
+    # which would otherwise break the fitting.
+    if ds.min().isnull():
+        return xr.DataArray(np.nan)
+    else:
+        x = np.arange(0, len(ds), 1)
+        coefs = poly.polyfit(x, ds, 4)
+        line_fit = poly.polyval(x, coefs)
+        detrended = (ds - line_fit)
+        return xr.DataArray(detrended)
+
+def remove_linear_fit(ds):
+    """
+    Removes a linear fit from an xarray dataset.
+    Returns the detrended timeseries.
+
+    Parameters
+    ----------
+    ds : xarray Dataset
+
+    Returns:
+    -------
+    detrended : xarray DataArray
+
+    Examples
+    --------
+    """
+    # Deals with cases where it is a NaN time series
+    # which would otherwise break the fitting.
+    if ds.min().isnull():
+        return xr.DataArray(np.nan.)
+    else:
+        x = np.arange(0, len(ds), 1)
+        coefs = poly.polyfit(x, ds, 1)
+        line_fit = poly.polyval(x, coefs)
+        detrended = (ds - line_fit)
+        return xr.DataArray(detrended)

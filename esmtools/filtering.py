40f4a511c6e17e6734eea7a20eade4ed80bd441a
Riley X. Brady <riley.brady@colorado.edu>
2018-10-03 16:48:31 -0600

    update filtering to add region extraction script

--- esmtools/filtering.py
+++ esmtools/filtering.py
@@ -3,13 +3,13 @@ Objects dealing with any sort of spatial and temporal filtering of output.
 
 Spatial
 -------
-- `find_indices` -- Find indices for a given lat/lon point on a meshgrid.
-
+- `_find_indices` -- Find indices for a given lat/lon point on a meshgrid.
+- `extract_region` -- Directly extracts a subset of a dataset.
 """
 
 import numpy as np
 
-def find_indices(xgrid, ygrid, xpoint, ypoint):
+def _find_indices(xgrid, ygrid, xpoint, ypoint):
     """
     Returns the i,j index for a latitude/longitude point on a grid.
     
@@ -38,7 +38,7 @@ def find_indices(xgrid, ygrid, xpoint, ypoint):
     >>> xx, yy = np.meshgrid(x, y)
     >>> xp = 20
     >>> yp = -20
-    >>> i, j = et.filtering.find_indices(x, y, xp, yp)
+    >>> i, j = et.filtering.find_indices(xx, yy, xp, yp)
     >>> print(xx[i, j])
     20.0
     >>> print(yy[i, j])
@@ -51,3 +51,32 @@ def find_indices(xgrid, ygrid, xpoint, ypoint):
     min_ix = np.nanargmin(reduced_grid)
     i, j = np.unravel_index(min_ix, reduced_grid.shape)
     return i, j
+
+def extract_region(ds, xgrid, ygrid, coords, lat_dim='nlat', lon_dim='nlon'):
+    """
+    Takes in an array of data, its lon/lat grid, and coordinates pertaining
+    to the lat/lon sub-box desired and returns the extracted data. 
+
+    Parameters
+    ----------
+    ds : array_like
+        Data to extract sub-region from. Ideally dataset.
+    xgrid, ygrid : array_like
+        Longitude and latitude meshgrid.
+    coords : vector
+        [x0, x1, y0, y1] pertaining to corners of box to extract
+    lat_dim, lon_dim : str (optional)
+        
+
+    Return
+    ------
+    subset_data : array_like
+        Data subset to domain of interest
+    """
+    print("NOTE: Make sure your coordinates are in order [x0, x1, y0, y1]")
+    x0, x1, y0, y1 = coords
+    a, c = _find_indices(lon, lat, x0, y0)
+    b, d = _find_indices(lon, lat, x1, y1)
+    subset_data = ds.isel(nlat=slice(a, b), nlon=slice(c, d))
+    return subset_data
+

9d8ba549cabd6eb37a8e1b550fc3ee0ba51e68ea
Riley X. Brady <riley.brady@colorado.edu>
2018-12-17 10:34:07 -0700

    Update package setup, requirements, etc.
    Former-commit-id: 76a5bbd8673053cd01740328a42ea09c84ab8895

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 from setuptools import find_packages, setup
 
-setup(name='esmtools',
-      version='0.1',
-      description='Toolbox for analyzing ESM output, with an emphasis on ocean model output',
-      url='http://github.com/bradyrx/esmtools',
-      author='Riley X. Brady',
-      author_email='riley.brady@colorado.edu',
-      license='MIT',
+DISTNAME = 'esmtools'
+VERSION = '0.1'
+AUTHOR = 'Riley X. Brady'
+AUTHOR_EMAIL = 'riley.brady@colorado.edu'
+DESCRIPTION = 'Tools for analyzing model output from ESMs, with a focus on ocean models and decadal prediction.'
+URL = 'https://github.com/bradyrx/esmtools'
+LICENSE = 'MIT'
+INSTALL_REQUIRES = ['xarray', 'pandas', 'numpy', 'matplotlib', 'pyfinance', 'xskillscore==0.0.2']
+DEPENDENCY_LINKS = ['https://github.com/raybellwaves/xskillscore/tarball/master#egg=xskillscore-0.0.2']
+# TODO: Add testing
+# TESTS_REQUIRE = ['pytest']
+PYTHON_REQUIRE = '>=3.6'
+
+setup(name=DISTNAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=open('README.md').read(),
+      url=URL,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      license=LICENSE,
       packages=find_packages(),
-      dependency_links=['https://github.com/raybellwaves/xskillscore'],
+      # NOTE: This will be deprecated, so either need to move away from non-pypi packages or find another solution.
+      dependency_links=DEPENDENCY_LINKS,
       # Needed for dependencies. Currently do not like the pyfinance or xskillscore dependency.
-      install_requires = ['xarray',
-                          'pandas',
-                          'numpy', 
-                          'pyfinance'],
-      zip_safe=False
+      install_requires=INSTALL_REQUIRES,
+      python_requires=PYTHON_REQUIRE
      )

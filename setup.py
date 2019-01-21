2dd744261dba15a1d1eaa8030488726b975ae712
Riley X. Brady <riley.brady@colorado.edu>
2019-01-21 09:04:11 -0700

    remove dependency link; xskillscore now on PyPI
    Former-commit-id: 23cc7bc1504511430ecb92ad74c663716829b2d2

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ AUTHOR_EMAIL = 'riley.brady@colorado.edu'
 DESCRIPTION = 'An xarray wrapper for decadal climate prediction.'
 URL = 'https://github.com/bradyrx/climpred'
 LICENSE = 'MIT'
-INSTALL_REQUIRES = ['numpy', 'pandas', 'xarray', 'scipy', 'xskillscore==0.0.2']
-DEPENDENCY_LINKS = ['https://github.com/aaronspring/xskillscore/tarball/master#egg=xskillscore-0.0.2']
+INSTALL_REQUIRES = ['numpy', 'pandas', 'xarray', 'scipy', 'xskillscore']
 # TODO: Add testing
 # TESTS_REQUIRE = ['pytest']
 PYTHON_REQUIRE = '>=3.6'
@@ -23,7 +22,6 @@ setup(name=DISTNAME,
       license=LICENSE,
       packages=find_packages(),
       # NOTE: This will be deprecated, so either need to move away from non-pypi packages or find another solution.
-      dependency_links=DEPENDENCY_LINKS,
       # Needed for dependencies. Currently do not like the pyfinance or xskillscore dependency.
       install_requires=INSTALL_REQUIRES,
       python_requires=PYTHON_REQUIRE

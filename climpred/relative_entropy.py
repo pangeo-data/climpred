e70d2ebecc9309c3ca0f2ccffc327bc87ad86434
Aaron Spring <aaron.spring@gmx.de>
2019-02-04 18:06:39 +0100

    changes after first review

--- climpred/relative_entropy.py
+++ climpred/relative_entropy.py
@@ -46,80 +46,94 @@ def _relative_entropy_formula(sigma_b, sigma_x, mu_x, mu_b, neofs):
     return R, dispersion, signal
 
 
-def _gen_control_ensemble(ds, control, it=10):
+def _gen_control_distribution(ds, control, it=10):
     """
-    Generate a large control ensemble of the control PDF.
+    Generate a large control distribution from control.
 
     Args:
-        ds (xr.DataArray): ensemble data with dimensions ensemble, member, time,
-                                lon (x), lat(y)
+        ds (xr.DataArray): initialization data with dimensions initialization,
+                           member, time, lon (x), lat(y).
         control (xr.DataArray): control data with dimensions time, lon (x),
-                                lat(y)
-        it (int): multiplying factor for ds.member
+                                lat(y).
+        it (int): multiplying factor for ds.member.
 
     Returns:
-        control_ensemble (xr.DataArray): data with dimensions ensemble, member,
-                                         time, lon (x), lat(y)
+        control_uninitialized (xr.DataArray): data with dimensions
+                                              initialization, member, time, lon
+                                              (x), lat(y).
 
     """
     ds_list = []
     for _ in range(it):
-        control_ensemble = _pseudo_ens(ds, control)
-        control_ensemble['ensemble'] = ds.ensemble.values
-        ds_list.append(control_ensemble)
-    control_ensemble = xr.concat(ds_list, 'member')
-    control_ensemble['member'] = np.arange(control_ensemble.member.size)
-    print('control_ensemble', control_ensemble.nbytes / 1e6, 'MB', 'member',
-          control_ensemble.member.size)
-    return control_ensemble
+        control_uninitialized = _pseudo_ens(ds, control)
+        control_uninitialized['initialization'] = ds.initialization.values
+        ds_list.append(control_uninitialized)
+    control_uninitialized = xr.concat(ds_list, 'member')
+    control_uninitialized['member'] =
+        np.arange(control_uninitialized.member.size)
+    return control_uninitialized
 
 
-def compute_relative_entropy(ds, control, control_ensemble, neofs=5,
-                             curv=True, ntime=10):
+def compute_relative_entropy(initialized, control_uninitialized,
+                             anomaly_data=False, neofs=None, curv=True,
+                             ntime=None):
     """
     Compute relative entropy.
 
-    Create anomalies. Calculates EOFs. Projects fields on EOFs to receive
-    pseudo-Principle Components per ensemble and lead year. Calculate relative
-    entropy based on _relative_entropy_formula.
+    Calculates EOFs from anomalies. Projects fields on EOFs to receive
+    pseudo-Principle Components per initialization and lead year. Calculate
+    relative entropy based on _relative_entropy_formula.
 
     Args:
-        ds (xr.DataArray): ensemble data with dimensions ensemble, member, time,
-                           lon (x), lat(y)
-        control (xr.DataArray): control data with dimensions time, lon (x),
-                                lat(y)
-        control_ensemble (xr.DataArray): control distribution with dimensions
-                                         ensemble, member, time, lon (x), lat(y)
-        neofs (int): number of EOFs to use
-        curv (bool): if curvilinear grids are provided disables EOF weights
-        ntime (int): number of timesteps calculated
+        initialized (xr.DataArray): anomaly ensemble data with dimensions
+                                    initialization, member, time, lon (x),
+                                    lat(y).
+        control_uninitialized (xr.DataArray): anomaly control distribution with
+                                              dimensions, initialization,
+                                              member, time, lon (x), lat(y).
+        anomaly_data (bool): Input data is anomaly alread. Default: False.
+        neofs (int): number of EOFs to use. Default: initialized.member.size.
+        curv (bool): if curvilinear grids are provided disables EOF weights.
+        ntime (int): number of timesteps calculated.
 
     Returns:
         rel_ent (pd.DataFrame): relative entropy
 
     """
-    anom_x = ds - control.mean('time')
-    anom_b = control_ensemble - control.mean('time')
-    anom = control - control.mean('time')
-    ensembles = ds.ensemble.values
-    length = ds.time.size
-    iterables = [['R', 'S', 'D'], ensembles]
+    if neofs is None:
+        neofs = initialized.member.size
+    if ntime is None:
+        ntime = initialized.time.size
+
+    if not anomaly_data:  # if ds, control are raw values
+        if detrend_by_control_time_mean:
+        anom_x = initialized - control.mean('time')
+        anom_b = control_uninitialized - control.mean('time')
+    else:  # leave as is when already anomalies
+        anom_x = initialized
+        anom_b = control_uninitialized
+
+    initializations = initialized.initialization.values
+    length = initialized.time.size
+    iterables = [['R', 'S', 'D'], initializations]
     mindex = pd.MultiIndex.from_product(
-        iterables, names=['component', 'ensemble'])
+        iterables, names=['component', 'initialization'])
     tt = np.arange(1, length)
     rel_ent = pd.DataFrame(index=tt, columns=mindex)
     rel_ent.index.name = 'Lead Year'
-
-    coslat = np.cos(np.deg2rad(anom_x.coords['lat'].values))
-    wgts = np.sqrt(coslat)[..., np.newaxis]
-    if curv:
+    if curv:  # if curvilinear lon(x,y), lat(x,y) data inputs
         wgts = None
-    solver = Eof(anom, weights=wgts)
+    else:
+        coslat = np.cos(np.deg2rad(anom_x.coords['lat'].values))
+        wgts = np.sqrt(coslat)[..., np.newaxis]
+    base_to_calc_eofs = control_uninitialized.stack(
+            new=('member','initialization')).rename({'new'";time"})
+    solver = Eof(base_to_calc_eofs, weights=wgts)
 
-    for ens in ensembles:
-        for t in ds.time.values[:ntime]:
+    for init in initializations:
+        for t in initialized.time.values[:ntime]:
             # P_b base distribution
-            pc_b = solver.projectField(anom_b.sel(ensemble=ens, time=t)
+            pc_b = solver.projectField(anom_b.sel(initialization=init, time=t)
                                              .drop('time')
                                              .rename({'member': 'time'}),
                                        neofs=neofs, eofscaling=0, weighted=False)
@@ -127,8 +141,8 @@ def compute_relative_entropy(ds, control, control_ensemble, neofs=5,
             mu_b = pc_b.mean('time')
             sigma_b = xr.DataArray(np.cov(pc_b.T))
 
-            # P_x ensemble distribution
-            pc_x = solver.projectField(anom_x.sel(ensemble=ens, time=t)
+            # P_x initialization distribution
+            pc_x = solver.projectField(anom_x.sel(initialization=init, time=t)
                                              .drop('time')
                                              .rename({'member': 'time'}),
                                        neofs=neofs, eofscaling=0, weighted=False)
@@ -145,38 +159,46 @@ def compute_relative_entropy(ds, control, control_ensemble, neofs=5,
     return rel_ent
 
 
-def bootstrap_relative_entropy(ds, control, control_ensemble, sig=95,
-                               bootstrap=100, curv=True, neofs=5):
+def bootstrap_relative_entropy(initialized, control_uninitialized, sig=95,
+                               bootstrap=100, curv=True, neofs=None,
+                               ntime=None):
     """
     Bootstrap relative entropy threshold.
 
-    Generates a random uninitialized ensemble and calculates the relative
+    Generates a random uninitialized initialization and calculates the relative
     entropy. sig-th percentile determines threshold level.
 
     Args:
-        ds (xr.DataArray): ensemble data with dimensions ensemble, member, time,
-                           lon (x), lat(y)
-        control (xr.DataArray): control data with dimensions time, lon (x),
-                                lat(y)
-        control_ensemble (xr.DataArray): control distribution with dimensions
-                                         ensemble, member, time, lon (x), lat(y)
-        sig (int): significance level for threshold
-        bootstrap (int): number of bootstrapping iterations
-        neofs (int): number of EOFs to use
-        curv (bool): if curvilinear grids are provided disables EOF weights
+        initialized (xr.DataArray): initialized ensemble with dimensions
+                                    initialization, member, time, lon (x),
+                                    lat(y).
+        control_uninitialized (xr.DataArray): control distribution with
+                                              dimensions initialization,
+                                              member, time, lon (x), lat(y).
+        sig (int): significance level for threshold.
+        bootstrap (int): number of bootstrapping iterations.
+        neofs (int): number of EOFs to use. Default: initialized.member.size
+        ntime (int): number of timestep to calculate.
+                     Default: initialized.time.size.
+        curv (bool): if curvilinear grids are provided disables EOF weights.
 
     Returns:
-        rel_ent (pd.DataFrame): relative entropy sig-th percentile threshold
+        rel_ent (pd.DataFrame): relative entropy sig-th percentile threshold.
 
     """
+    if neofs is None:
+        neofs = initialized.member.size
+    if ntime is None:
+        ntime = initialized.time.size
+
     x = []
-    for _ in range(min(1, int(bootstrap / ds['time'].size))):
-        ds_control = _pseudo_ens(ds, control)
-        ds_control['ensemble'] = ds.ensemble.values
+    for _ in range(min(1, int(bootstrap / initialized.time.size))):
+        ds_control = _pseudo_ens(initialized, control)
+        ds_control['initialization'] = initialized.initialization.values
         ds_control['member'] = np.arange(ds_control.member.size)
         ds_pseudo_rel_ent = compute_relative_entropy(
-            ds_control, control, control_ensemble, neofs=neofs, curv=curv,
-            ntime=ds.time.size)
+            ds_control, control_uninitialized, neofs=neofs, curv=curv,
+            ntime=ntime)
         x.append(ds_pseudo_rel_ent)
     ds_pseudo_metric = pd.concat(x, ignore_index=True)
     qsig = sig / 100
@@ -200,7 +222,7 @@ def plot_relative_entropy(rel_ent, rel_ent_threshold=None, **kwargs):
         m = rel_ent[dim].median(axis=1)
         std = rel_ent[dim].std(axis=1)
         ax[i].plot(rel_ent[dim], c='gray',
-                   label='individual ensembles', linewidth=.5, alpha=.5)
+                   label='individual initializations', linewidth=.5, alpha=.5)
         ax[i].plot(m, c=colors[i], label=dim, linewidth=2.5)
         ax[i].plot((m - std), c=colors[i], label=dim + ' median +/- std',
                    linewidth=2.5, ls='--')

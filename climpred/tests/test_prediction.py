67f3f8732abb6fbcb20cf6c03ba6f252bd699fd6
AS <aaron.spring@mpimet.mpg.de>
2019-04-15 13:32:14 +0200

    adapted persistence_pm and its tests for datetime and monthly, pers_sig
    Former-commit-id: 05fa484b8865ba1a2cede1f437840f22affe3767

--- climpred/tests/test_prediction.py
+++ climpred/tests/test_prediction.py
@@ -3,22 +3,24 @@ import pandas as pd
 import pytest
 import xarray as xr
 
-from climpred.prediction import compute_perfect_model, compute_persistence
 from climpred.bootstrap import bootstrap_perfect_model
+from climpred.prediction import compute_perfect_model, compute_persistence_pm
 
 xskillscore_metrics = ('pearson_r', 'rmse', 'mse', 'mae')
+xskillscore_distance_metrics = ('rmse', 'mse', 'mae')
 PM_only_metrics = ('nrmse', 'nmse', 'nmae')  # excl uacc because sqrt(neg)
 PM_comparisons = ('e2c', 'm2c', 'm2e', 'm2m')
 all_metrics = xskillscore_metrics + PM_only_metrics
+all_metrics_wo_pearson_r = xskillscore_distance_metrics + PM_only_metrics
 
 
 @pytest.fixture
 def PM_da_ds():
-    dates = pd.date_range('1/1/2000', '1/3/2000', freq='D')
+    dates = pd.date_range('1/1/2000', '3/1/2000', freq='M')
     lats = np.arange(4)
     lons = np.arange(3)
     member = np.arange(3)
-    initialization = np.arange(3)
+    initialization = [2000, 2000]
     data = np.random.rand(len(dates), len(
         lats), len(lons), len(member), len(initialization))
     return xr.DataArray(data,
@@ -29,7 +31,7 @@ def PM_da_ds():
 
 @pytest.fixture
 def PM_da_control():
-    dates = pd.date_range('1/1/2000', '1/30/2000', freq='D')
+    dates = pd.date_range('1/1/2000', periods=30, freq='M')
     lats = np.arange(4)
     lons = np.arange(3)
     data = np.random.rand(len(dates), len(lats), len(lons))
@@ -40,11 +42,11 @@ def PM_da_control():
 
 @pytest.fixture
 def PM_ds_ds():
-    dates = pd.date_range('1/1/2000', '1/3/2000', freq='D')
+    dates = pd.date_range('1/1/2000', '3/1/2000', freq='M')
     lats = np.arange(4)
     lons = np.arange(3)
     member = np.arange(3)
-    initialization = np.arange(3)
+    initialization = [2000, 2000]
     data = np.random.rand(len(dates), len(
         lats), len(lons), len(member), len(initialization))
     return xr.Dataset({'varname1': (['time', 'lat', 'lon', 'member',
@@ -58,7 +60,7 @@ def PM_ds_ds():
 
 @pytest.fixture
 def PM_ds_control():
-    dates = pd.date_range('1/1/2000', '1/30/2000', freq='D')
+    dates = pd.date_range('1/1/2000', periods=30, freq='M')
     lats = np.arange(4)
     lons = np.arange(3)
     data = np.random.rand(len(dates), len(lats), len(lons))
@@ -105,15 +107,15 @@ def test_compute_perfect_model_ds_not_nan(PM_ds_ds, PM_ds_control, metric,
 #    assert actual == False
 
 
-@pytest.mark.parametrize('metric', xskillscore_metrics)
-def test_compute_persistence_da_not_nan(PM_da_ds, PM_da_control, metric):
-    actual = compute_persistence(
-        PM_da_ds, PM_da_control, nlags=2, metric=metric, dim='time').isnull().any()
+@pytest.mark.parametrize('metric', xskillscore_distance_metrics)
+def test_compute_persistence_pm_da_not_nan(PM_da_ds, PM_da_control, metric):
+    actual = compute_persistence_pm(
+        PM_da_ds, PM_da_control, 2, metric=metric, dim='time').isnull().any()
     assert actual == False
 
 
-@pytest.mark.parametrize('metric', xskillscore_metrics)
-def test_compute_persistence_ds_not_nan(PM_ds_ds, PM_ds_control, metric):
-    actual = compute_persistence(
-        PM_ds_ds, PM_ds_control, nlags=2, metric=metric, dim='time').isnull().any()
+@pytest.mark.parametrize('metric', xskillscore_distance_metrics)
+def test_compute_persistence_pm_ds_not_nan(PM_ds_ds, PM_ds_control, metric):
+    actual = compute_persistence_pm(
+        PM_ds_ds, PM_ds_control, 2, metric=metric, dim='time').isnull().any()
     assert actual == False

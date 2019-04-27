3b5261e464891b48e880e1e2cc6a88d1753d4c3f
Riley X. Brady <riley.brady@colorado.edu>
2019-04-27 13:41:27 -0600

    update changelog and version to 0.3 with release of object-oriented system
    Former-commit-id: 556767ad1d911b469395b3cce1e6fba4b02388b3 [formerly fad2eba5d6b2fb314614ff4c1db85ca484324064]
    Former-commit-id: 36a5265130a37cda9c23b76b4fcfb7b6d51c984d

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import find_packages, setup
 
 DISTNAME = 'climpred'
-VERSION = '0.2'
+VERSION = '0.3'
 AUTHOR = 'Riley X. Brady'
 AUTHOR_EMAIL = 'riley.brady@colorado.edu'
 DESCRIPTION = 'An xarray wrapper for analysis of ensemble forecast models for climate prediction.'
 URL = 'https://github.com/bradyrx/climpred'
 LICENSE = 'MIT'
 INSTALL_REQUIRES = ['numpy', 'pandas',
-                    'xarray', 'scipy', 'xskillscore', 'eofs', 'cftime']
+                    'xarray', 'scipy',
+                    'xskillscore', 'eofs',
+                    'cftime']
 TESTS_REQUIRE = ['pytest']
 PYTHON_REQUIRE = '>=3.6'
 
@@ -21,8 +23,6 @@ setup(name=DISTNAME,
       author_email=AUTHOR_EMAIL,
       license=LICENSE,
       packages=find_packages(),
-      # NOTE: This will be deprecated, so either need to move away from non-pypi packages or find another solution.
-      # Needed for dependencies.
       install_requires=INSTALL_REQUIRES,
       python_requires=PYTHON_REQUIRE,
       tests_require=TESTS_REQUIRE,

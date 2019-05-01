6851c0073ca4019c7501352cebabdab37756e039
AS <aaron.spring@mpimet.mpg.de>
2019-05-01 17:34:55 +0200

    fixed bootstrap_perfect_model for xskillscore metrics

--- climpred/tests/test_perfect_model_prediction.py
+++ climpred/tests/test_perfect_model_prediction.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pytest
-import xarray as xr
 
-from climpred.bootstrap import bootstrap_perfect_model, pseudo_ens
+from climpred.bootstrap import bootstrap_perfect_model
 from climpred.loadutils import open_dataset
 from climpred.prediction import compute_perfect_model, compute_persistence_pm
 
@@ -25,6 +24,10 @@ def PM_da_ds3d():
 def PM_da_control3d():
     da = open_dataset('MPI-control-3D')
     da = da.isel(x=slice(0, 50), y=slice(125, 150))
+    # fix to span 300yr control
+    t = list(np.arange(da.time.size))
+    da = da.isel(time=t*6)
+    da['time'] = np.arange(3000, 3000 + da.time.size)
     return da['tos']
 
 
@@ -39,6 +42,9 @@ def PM_ds_ds3d():
 def PM_ds_control3d():
     ds = open_dataset('MPI-control-3D')
     ds = ds.isel(x=slice(0, 50), y=slice(125, 150))
+    t = list(np.arange(ds.time.size))
+    ds = ds.isel(time=t*6)
+    ds['time'] = np.arange(3000, 3000 + ds.time.size)
     return ds
 
 
@@ -66,51 +72,6 @@ def PM_ds_ds1d():
 def PM_ds_control1d():
     ds = open_dataset('MPI-control-1D')
     return ds
-
-
-# v = 'tos'
-# ds = open_dataset('MPI-PM-DP-3D')  # [v]
-# control = open_dataset('MPI-control-3D')  # [v]
-# these three lines fix short control
-#t = list(np.arange(control.time.size))
-#control = control.isel(time=t*6)
-#control['time'] = np.arange(3000, 3000+control.time.size)
-
-
-#res = compute_perfect_model(ds, control, metric='rmse', comparison='m2m')
-
-
-# res = bootstrap_perfect_model(
-#    ds, control, metric='pearson_r', comparison='m2e', bootstrap=2, compute_persistence_skill=True)
-
-# init_skill = compute_perfect_model(
-#    ds, control, metric='pearson_r', comparison='m2e')
-#pseudo_ds = pseudo_ens(ds, control)
-# uninit_skill = compute_perfect_model(
-#    pseudo_ds, control, metric='pearson_r', comparison='m2e')
-# pers_skill = compute_perfect_model(
-#    ds, control, metric='pearson_r', comparison='m2e')
-
-#skill = xr.concat([init_skill, uninit_skill, pers_skill], 'i')
-#skill['i'] = ['init', 'uninit', 'pers']
-
-#init_ci, uninit_ci, pers_ci, p_uninit_over_init, p_pers_over_init = res
-
-# p = xr.concat(
-#    [i for i in [p_uninit_over_init, p_pers_over_init] if i is not None], 'i')
-# what if one above is None?
-# ['i'] = ['uninit', 'pers']
-
-
-# ci = xr.concat([init_ci, uninit_ci, pers_ci],
-#               'i').rename({'quantile': 'results'})
-#ci['i'] = ['init', 'uninit', 'pers']
-
-
-#results = xr.concat([skill, p], 'results')
-#results['results'] = ['skill', 'p']
-#results = xr.concat([results, ci], 'results')
-# results.coords
 
 
 @pytest.mark.parametrize('comparison', PM_comparisons)
@@ -142,8 +103,8 @@ def test_compute_perfect_model_ds_not_nan(PM_ds_ds3d, PM_ds_control3d, metric,
 
 
 @pytest.mark.parametrize('metric', xskillscore_metrics)
-def test_compute_persistence_pm_ds_not_nan(PM_ds_ds1d, PM_ds_control1d,
-                                           metric):
+def test_compute_persistence_pm_ds1d_not_nan(PM_ds_ds1d, PM_ds_control1d,
+                                             metric):
     """
     Checks that there are no NaNs on persistence forecast of 1D time series.
     """
@@ -153,10 +114,9 @@ def test_compute_persistence_pm_ds_not_nan(PM_ds_ds1d, PM_ds_control1d,
         assert not actual[var]
 
 
-# TODO: should work for pearson_r as well, does when more than one initialization present
 @pytest.mark.parametrize('metric', xskillscore_distance_metrics)
-def test_compute_persistence_pm_ds_not_nan(PM_ds_ds3d, PM_ds_control3d,
-                                           metric):
+def test_compute_persistence_pm_ds3d_not_nan(PM_ds_ds3d, PM_ds_control3d,
+                                             metric):
     """
     Checks that there are no NaNs on persistence forecast of 1D time series.
     """
@@ -166,21 +126,80 @@ def test_compute_persistence_pm_ds_not_nan(PM_ds_ds3d, PM_ds_control3d,
         assert not actual[var]
 
 
-# NOTE: bootstrap_perfect_model breaks on 'm2m'. It should be added back into
-# the test once that is resolved.
+# NOTE:
 # Also, it breaks when any PM-only metrics are
 # used, since `compute_persistence_pm` breaks on non-xskillscore metrics. [resolved]
-@pytest.mark.parametrize('comparison', ('e2c', 'm2c', 'm2e'))
-@pytest.mark.parametrize('metric', all_metrics)
-def test_bootstrap_perfect_model_da_not_nan(PM_da_ds1d, PM_da_control1d,
-                                            metric, comparison):
+@pytest.mark.parametrize('comparison', (PM_comparisons))
+@pytest.mark.parametrize('metric', xskillscore_metrics)
+def test_bootstrap_perfect_model_da1d_not_nan(PM_da_ds1d, PM_da_control1d,
+                                              metric, comparison):
     """
-    Checks that there are no NaNs on bootstrap of 1D time series.
+    Checks that there are no NaNs on bootstrap init skill or uninit p of 1D da time series.
     """
     actual = bootstrap_perfect_model(PM_da_ds1d, PM_da_control1d,
                                      metric=metric, comparison=comparison,
                                      sig=50,
-                                     compute_persistence_skill=False,
-                                     bootstrap=2).isnull().any()
+                                     bootstrap=2)
+    actual_init_skill = actual.sel(i='init', results='skill').isnull().any()
+    assert not actual_init_skill
+    actual_uninit_p = actual.sel(i='uninit', results='p').isnull().any()
+    assert not actual_uninit_p
+
+
+@pytest.mark.parametrize('comparison', (PM_comparisons))
+@pytest.mark.parametrize('metric', xskillscore_metrics)
+def test_bootstrap_perfect_model_ds1d_not_nan(PM_ds_ds1d, PM_ds_control1d,
+                                              metric, comparison):
+    """
+    Checks that there are no NaNs on bootstrap init skill or uninit p of 1D ds time series.
+    """
+    actual = bootstrap_perfect_model(PM_ds_ds1d, PM_ds_control1d,
+                                     metric=metric, comparison=comparison,
+                                     sig=50,
+                                     bootstrap=2)
     for var in actual.data_vars:
-        assert not actual[var]
+        actual_init_skill = actual[var].sel(
+            i='init', results='skill').isnull().any()
+        assert not actual_init_skill
+    for var in actual.data_vars:
+        actual_uninit_p = actual[var].sel(
+            i='uninit', results='p').isnull().any()
+        assert not actual_uninit_p
+
+
+@pytest.mark.parametrize('comparison', (PM_comparisons))
+@pytest.mark.parametrize('metric', xskillscore_metrics)
+def test_bootstrap_perfect_model_da3d_not_nan(PM_da_ds3d, PM_da_control3d,
+                                              metric, comparison):
+    """
+    Checks that there are no NaNs on bootstrap init skill or uninit p of 3D da.
+    """
+    actual = bootstrap_perfect_model(PM_da_ds3d, PM_da_control3d,
+                                     metric=metric, comparison=comparison,
+                                     sig=50,
+                                     bootstrap=2)
+    actual_init_skill = actual.sel(i='init', results='skill').isnull().any()
+    assert not actual_init_skill
+    actual_uninit_p = actual.sel(i='uninit', results='p').isnull().any()
+    assert not actual_uninit_p
+
+
+@pytest.mark.parametrize('comparison', (PM_comparisons))
+@pytest.mark.parametrize('metric', xskillscore_metrics)
+def test_bootstrap_perfect_model_ds3d_not_nan(PM_ds_ds3d, PM_ds_control3d,
+                                              metric, comparison):
+    """
+    Checks that there are no NaNs on bootstrap init skill or uninit p of 3D ds.
+    """
+    actual = bootstrap_perfect_model(PM_ds_ds3d, PM_ds_control3d,
+                                     metric=metric, comparison=comparison,
+                                     sig=50,
+                                     bootstrap=2)
+    for var in actual.data_vars:
+        actual_init_skill = actual[var].sel(
+            i='init', results='skill').isnull().any()
+        assert not actual_init_skill
+    for var in actual.data_vars:
+        actual_uninit_p = actual[var].sel(
+            i='uninit', results='p').isnull().any()
+        assert not actual_uninit_p

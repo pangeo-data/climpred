3b1ffc870ed59ba64f78336318398b49e4bd186a
Aaron Spring <aaronspring@users.noreply.github.com>
2019-05-03 19:21:44 +0200

    Update comparisons (#107)
    * sshfs annoying tmp files
    
    * create unitests for all comparisons. change _m2e to remove ref from ens
    
    *  reduced 3d area to increase speed
    
    
    
    Former-commit-id: 6af09b0a892cbbe88c173bd164a4e244ce24f819

--- climpred/tests/test_comparisons.py
+++ climpred/tests/test_comparisons.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pytest
 import xarray as xr
-from climpred.comparisons import _drop_members, _m2m
+from xarray.testing import assert_equal
+
+from climpred.comparisons import (_drop_members, _e2c, _m2c, _m2e, _m2m,
+                                  _stack_to_supervector)
 from climpred.loadutils import open_dataset
 
 
@@ -19,20 +22,108 @@ def PM_da_control1d():
     return da
 
 
-@pytest.fixture
-def PM_ds_ds1d():
-    ds = open_dataset('MPI-PM-DP-1D')
-    return ds
+def m2e(ds, supervector_dim='svd'):
+    reference_list = []
+    forecast_list = []
+    for m in ds.member.values:
+        forecast = _drop_members(ds, rmd_member=[m]).mean('member')
+        reference = ds.sel(member=m).squeeze()
+        forecast, reference = xr.broadcast(forecast, reference)
+        forecast_list.append(forecast)
+        reference_list.append(reference)
+    reference = xr.concat(reference_list,
+                          'init').rename({'init': supervector_dim})
+    forecast = xr.concat(forecast_list,
+                         'init').rename({'init': supervector_dim})
+    return forecast, reference
 
 
-@pytest.fixture
-def PM_ds_control1d():
-    ds = open_dataset('MPI-control-1D')
-    return ds
+def test_e2c(PM_da_ds1d):
+    """Test ensemble_mean-to-control (which can be any other one member) (e2c) comparison basic functionality.
+
+    Clean comparison: Remove one control member from ensemble to use as reference. Take the remaining member mean as forecasts."""
+    ds = PM_da_ds1d
+    aforecast, areference = _e2c(ds)
+
+    control_member = [0]
+    supervector_dim = 'svd'
+    reference = ds.isel(member=control_member).squeeze()
+    if 'member' in reference.coords:
+        del reference['member']
+    reference = reference.rename({'init': supervector_dim})
+    # drop the member being reference
+    ds = _drop_members(ds, rmd_member=[ds.member.values[control_member]])
+    forecast = ds.mean('member')
+    forecast = forecast.rename({'init': supervector_dim})
+
+    eforecast, ereference = forecast, reference
+    # very weak testing on shape
+    assert eforecast.size == aforecast.size
+    assert ereference.size == areference.size
+
+    assert_equal(eforecast, aforecast)
+    assert_equal(ereference, areference)
+
+
+def test_m2c(PM_da_ds1d):
+    """Test many-to-control (which can be any other one member) (m2c) comparison basic functionality.
+
+    Clean comparison: Remove one control member from ensemble to use as reference. Take the remaining members as forecasts."""
+    ds = PM_da_ds1d
+    aforecast, areference = _m2c(ds)
+
+    supervector_dim = 'svd'
+    control_member = [0]
+    reference = ds.isel(member=control_member).squeeze()
+    # drop the member being reference
+    ds_dropped = _drop_members(ds, rmd_member=ds.member.values[control_member])
+    forecast, reference = xr.broadcast(ds_dropped, reference)
+    forecast = _stack_to_supervector(forecast, new_dim=supervector_dim)
+    reference = _stack_to_supervector(reference, new_dim=supervector_dim)
+
+    eforecast, ereference = forecast, reference
+    # very weak testing on shape
+    assert eforecast.size == aforecast.size
+    assert ereference.size == areference.size
+
+    assert_equal(eforecast, aforecast)
+    assert_equal(ereference, areference)
+
+
+def test_m2e(PM_da_ds1d):
+    """Test many-to-ensemble-mean (m2e) comparison basic functionality.
+
+    Clean comparison: Remove one member from ensemble to use as reference. Take the remaining members as forecasts."""
+    ds = PM_da_ds1d
+    aforecast, areference = _m2e(ds)
+
+    supervector_dim = 'svd'
+    reference_list = []
+    forecast_list = []
+    for m in ds.member.values:
+        forecast = _drop_members(ds, rmd_member=[m]).mean('member')
+        reference = ds.sel(member=m).squeeze()
+        forecast, reference = xr.broadcast(forecast, reference)
+        forecast_list.append(forecast)
+        reference_list.append(reference)
+    reference = xr.concat(reference_list,
+                          'init').rename({'init': supervector_dim})
+    forecast = xr.concat(forecast_list,
+                         'init').rename({'init': supervector_dim})
+
+    eforecast, ereference = forecast, reference
+    # very weak testing on shape
+    assert eforecast.size == aforecast.size
+    assert ereference.size == areference.size
+
+    assert_equal(eforecast, aforecast)
+    assert_equal(ereference, areference)
 
 
 def test_m2m(PM_da_ds1d):
-    "Test m2m basic functionality of many to many comparison"
+    """Test many-to-many (m2m) comparison basic functionality.
+
+    Clean comparison: Remove one member from ensemble to use as reference. Take the remaining members as forecasts."""
     ds = PM_da_ds1d
     aforecast, areference = _m2m(ds)
 
@@ -46,15 +137,14 @@ def test_m2m(PM_da_ds1d):
         for m2 in ds_reduced.member:
             for i in ds.init:
                 reference_list.append(reference.sel(init=i))
-                forecast_list.append(
-                    ds_reduced.sel(member=m2, init=i))
-    reference = xr.concat(
-        reference_list, supervector_dim)
-    reference[supervector_dim] = np.arange(1, 1+reference.svd.size)
-    forecast = xr.concat(
-        forecast_list, supervector_dim)
-    forecast[supervector_dim] = np.arange(1, 1+forecast.svd.size)
+                forecast_list.append(ds_reduced.sel(member=m2, init=i))
+    reference = xr.concat(reference_list, supervector_dim)
+    reference[supervector_dim] = np.arange(1, 1 + reference.svd.size)
+    forecast = xr.concat(forecast_list, supervector_dim)
+    forecast[supervector_dim] = np.arange(1, 1 + forecast.svd.size)
     eforecast, ereference = forecast, reference
-
+    # very weak testing here
     assert eforecast.size == aforecast.size
     assert ereference.size == areference.size
+    #assert_equal(eforecast,aforecast)
+    #assert_equal(ereference,areference)

3859761634ca95223b7ab5df87efc96f66ad3db8
AS <aaron.spring@mpimet.mpg.de>
2019-05-10 16:53:21 +0200

    cleanup

--- climpred/tests/test_loadutils.py
+++ climpred/tests/test_loadutils.py
@@ -1,10 +1,6 @@
 import os
 
-import numpy as np
-import pytest
 import xarray as xr
-
-from climpred.loadutils import get_datasets, open_dataset
 
 
 def test_open_dataset_locally():
@@ -12,7 +8,7 @@ def test_open_dataset_locally():
     from climpred.loadutils import file_alias_dict as datasets
     extension = 'sample_data/prediction'
     pwd = os.getcwd()
-    for dataset_name, dataset_nc in datasets.items():
+    for dataset_nc in datasets.values():
         f = pwd + '/' + extension + '/' + dataset_nc + '.nc'
         d = xr.open_mfdataset(f)
         assert d.nbytes > 1

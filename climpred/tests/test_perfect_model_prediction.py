1648ef27b420877233b09ecc654aef74a2671b40
AS <aaron.spring@mpimet.mpg.de>
2019-05-20 18:32:38 +0200

    adapt test_pm

--- climpred/tests/test_perfect_model_prediction.py
+++ climpred/tests/test_perfect_model_prediction.py
@@ -1,6 +1,6 @@
 import pytest
 
-from climpred.bootstrap import bootstrap_hindcast, bootstrap_perfect_model
+from climpred.bootstrap import bootstrap_perfect_model
 from climpred.comparisons import ALL_PM_COMPARISONS_DICT
 from climpred.loadutils import open_dataset
 from climpred.metrics import ALL_PM_METRICS_DICT
@@ -8,10 +8,7 @@ from climpred.prediction import compute_perfect_model, compute_persistence
 
 # TODO: comment out
 # faster with only two comparisons
-ALL_PM_COMPARISONS_DICT = {
-    'm2c': '',
-    'e2c': ''
-}
+ALL_PM_COMPARISONS_DICT = {'m2c': '', 'e2c': ''}
 
 
 @pytest.fixture
@@ -49,6 +46,20 @@ def test_compute_persistence_ds1d_not_nan(PM_ds_ds1d, PM_ds_control1d, metric):
                                  metric=metric).isnull().any()
     for var in actual.data_vars:
         assert not actual[var]
+
+
+@pytest.mark.parametrize('comparison', ALL_PM_COMPARISONS_DICT.keys())
+@pytest.mark.parametrize('metric', ALL_PM_METRICS_DICT.keys())
+def test_compute_perfect_model_da1d_not_nan(PM_da_ds1d, PM_da_control1d,
+                                            comparison, metric):
+    """
+    Checks that there are no NaNs on persistence forecast of 1D time series.
+    """
+    actual = compute_perfect_model(PM_da_ds1d,
+                                   PM_da_control1d,
+                                   comparison=comparison,
+                                   metric=metric).isnull().any()
+    assert not actual
 
 
 @pytest.mark.parametrize('comparison', ALL_PM_COMPARISONS_DICT.keys())

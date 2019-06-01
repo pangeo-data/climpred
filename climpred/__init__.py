7745a9eb4f1c4cca71766f566fa6eed4468dd6a9
Riley X. Brady <riley.brady@colorado.edu>
2019-06-01 14:17:25 -0600

    change loadutils to tutorial

--- climpred/__init__.py
+++ climpred/__init__.py
@@ -1,12 +1,11 @@
 from . import prediction
 from . import stats
-from . import loadutils
+from . import tutorial
 from . import relative_entropy
 from . import graphics
 from . import bootstrap
 from . import metrics
 from . import comparisons
-
 # eventually will *only* import these
 from .classes import PerfectModelEnsemble, HindcastEnsemble
 from . import exceptions

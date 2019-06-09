56cd721637651bff77fbe820505336d6904b93ec
Riley X. Brady <riley.brady@colorado.edu>
2019-06-08 21:08:00 -0600

    Move notebooks to documentation; add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,16 +10,20 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import datetime
+import os
+import sys
+
+import climpred
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'climpred'
-copyright = '2019, Riley X. Brady and Aaron Spring'
-author = 'Riley X. Brady and Aaron Spring'
+project = "climpred"
+copyright = "2019, Riley X. Brady and Aaron Spring"
+author = "Riley X. Brady and Aaron Spring"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,11 +31,10 @@ author = 'Riley X. Brady and Aaron Spring'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ["nbsphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -44,9 +47,14 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+#html_theme = "alabaster"
+html_theme = 'sphinx_rtd_theme'
+html_logo = 'images/climpred-logo.png'
+html_theme_options = {
+        'logo_only': True,
+        'style_nav_header_background': '#FFFFFF'
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,20 +1,17 @@
-.. climpred documentation master file, created by
-   sphinx-quickstart on Sat Jun  8 20:41:55 2019.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to climpred's documentation!
+climpred
 ====================================
+
+**User Guide**
+
+* :doc:`examples`
+
+.. toctree::
+    :maxdepth: 1
+    :hidden:
+    :caption: User guide
+
+    examples
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`

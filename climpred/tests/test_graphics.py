ad5c6d4b07b4e7ca6f7a2ccdfd62516b6cc7afdd
AS <aaron.spring@mpimet.mpg.de>
2020-04-06 11:59:15 +0200

    bootstrap carries lead attrs, graphics bs_plot more flexible

--- climpred/tests/test_graphics.py
+++ climpred/tests/test_graphics.py
@@ -3,23 +3,37 @@ from climpred.graphics import plot_bootstrapped_skill_over_leadyear
 from climpred.tutorial import load_dataset
 
 
-def test_mpi_pm_plot_bootstrapped_skill_over_leadyear():
+def test_mpi_pm_plot_bootstrapped_skill_over_leadyear_da():
     """
-    Checks plots from bootstrap_perfect_model works.
+    Checks plots from bootstrap_perfect_model works for xr.DataArray.
     """
-    da = load_dataset("MPI-PM-DP-1D").isel(area=1, period=-1)
-    PM_da_ds1d = da["tos"]
+    da = load_dataset('MPI-PM-DP-1D').isel(area=1, period=-1)
+    PM_da_ds1d = da['tos']
 
-    da = load_dataset("MPI-control-1D").isel(area=1, period=-1)
-    PM_da_control1d = da["tos"]
+    da = load_dataset('MPI-control-1D').isel(area=1, period=-1)
+    PM_da_control1d = da['tos']
 
     bootstrap = 3
     res = bootstrap_perfect_model(
-        PM_da_ds1d,
-        PM_da_control1d,
-        metric="pearson_r",
-        dim="init",
-        bootstrap=bootstrap,
-    ).mean("member")
-    res_ax = plot_bootstrapped_skill_over_leadyear(res, 95)
+        PM_da_ds1d, PM_da_control1d, metric='pearson_r', bootstrap=bootstrap,
+    )
+    res_ax = plot_bootstrapped_skill_over_leadyear(res)
+    assert res_ax is not None
+
+
+def test_mpi_pm_plot_bootstrapped_skill_over_leadyear_ds():
+    """
+    Checks plots from bootstrap_perfect_model works for xr.Dataset with one variable.
+    """
+    da = load_dataset('MPI-PM-DP-1D').isel(area=1, period=-1)
+    PM_da_ds1d = da['tos'].to_dataset(name='tos')
+
+    da = load_dataset('MPI-control-1D').isel(area=1, period=-1)
+    PM_da_control1d = da['tos'].to_dataset(name='tos')
+
+    bootstrap = 3
+    res = bootstrap_perfect_model(
+        PM_da_ds1d, PM_da_control1d, metric='pearson_r', bootstrap=bootstrap,
+    )
+    res_ax = plot_bootstrapped_skill_over_leadyear(res)
     assert res_ax is not None

0d72995b74000c8b5a15131111758aeeb9fd5f51
AS <aaron.spring@mpimet.mpg.de>
2020-08-27 17:19:49 +0200

    fix typo

--- climpred/classes.py
+++ climpred/classes.py
@@ -749,7 +749,7 @@ class PerfectModelEnsemble(PredictionEnsemble):
         self,
         metric='pearson_r',
         comparison='m2e',
-        dim=['member', 'init'],
+        dim=['init', 'member'],
         sig=95,
         iterations=500,
         pers_sig=None,

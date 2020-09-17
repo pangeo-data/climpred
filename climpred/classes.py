fef647b569f1e575c872eb8a105b51aafad26505
Aaron Spring <aaronspring@users.noreply.github.com>
2020-09-17 16:11:48 +0200

    Update classes.py

--- climpred/classes.py
+++ climpred/classes.py
@@ -1238,6 +1238,10 @@ class HindcastEnsemble(PredictionEnsemble):
             else:
                 raise NotImplementedError(f'{h}_bias_reduction is not implemented.')
 
-            self = func(self, alignment=alignment, cross_validate=cross_validate,
-                        **metric_kwargs)
+            self = func(
+                self,
+                alignment=alignment,
+                cross_validate=cross_validate,
+                **metric_kwargs
+            )
         return self

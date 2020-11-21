febde3cdc1ba3f3efc331a9ba70ac9e696ed135b
AS <aaron.spring@mpimet.mpg.de>
2020-11-21 22:06:28 +0100

    better docstrings and 3d test

--- climpred/predictability_horizon.py
+++ climpred/predictability_horizon.py
@@ -2,7 +2,20 @@ import numpy as np
 import xarray as xr
 
 
-def last_item_cond_true(cond, dim, shift=-1):
+def _last_item_cond_true(cond, dim, shift=-1):
+    """Get the last item of condition ``cond`` which is True.
+
+    Args:
+        cond (xr.DataArray, xr.Dataset): Condition True means predictable. ``cond``
+            contains at least the dimension lead and checks for how many leads the
+            ``condition`` is true and hence skill is predictable.
+        dim (str): Dimension to check for condition == True over.
+        shift (int): Description of parameter `shift`. Defaults to 1.
+
+    Returns:
+        xr.DataArray, xr.Dataset: ``dim`` value until condition is True.
+
+    """
     reached = cond.idxmin(dim) + shift  # to get the last True lead
     # fix below one
     reached = reached.where(reached >= 1, np.nan)
@@ -11,14 +24,34 @@ def last_item_cond_true(cond, dim, shift=-1):
     # reset where always true to max dim
     reached = reached.where(~cond.all("lead"), other=cond[dim].max())
     # fix locations where always nan to nan
-    mask = ~(cond == False).all("lead")
+    mask = cond.notnull().all("lead")  # ~(cond == False).all("lead")
     reached = reached.where(mask, other=np.nan)
     return reached
 
 
 def predictability_horizon(cond):
-    """Calculate the predictability horizon based on a condition `cond`."""
-    ph = last_item_cond_true(cond, "lead")
+    """Calculate the predictability horizon based on a condition ```cond``.
+
+    Args:
+        cond (xr.DataArray, xr.Dataset): Condition True means predictable. ``cond``
+        contains at least the dimension lead and checks for how many leads the
+        ``condition`` is true and hence skill is predictable.
+
+    Returns:
+        xr.DataArray, xr.Dataset: predictability horizon reduced by ``lead`` dimension.
+
+    Example:
+        >>> skill = pm.verify(metric='acc', comparison='m2e', dim=['init','member'],
+                reference=['persistence'])
+        >>> predictability_horizon(skill.sel(skill='initialized') >
+                skill.sel(skill='persistence'))
+
+        >>> bskill = pm.bootstrap(metric='acc', comparison='m2e', dim=['init','member'],
+                reference=['persistence'], iterations=21)
+        >>> predictability_horizon(bskill.sel(skill='persistence', results='p') <= 0.05)
+
+    """
+    ph = _last_item_cond_true(cond, "lead")
     if isinstance(ph, xr.DataArray):
         ph.attrs["units"] = cond["lead"].attrs["units"]
     elif isinstance(ph, xr.Dataset):
--- climpred/tests/test_predictability_horizon.py
+++ climpred/tests/test_predictability_horizon.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 import xarray as xr
 
-from climpred.predictability_horizon import last_item_cond_true, predictability_horizon
+from climpred.predictability_horizon import _last_item_cond_true, predictability_horizon
 
 
 @pytest.mark.parametrize("threshold,expected", [(1, 1), (2, 3), (3, 6), (3.5, 6)])
@@ -12,14 +12,14 @@ def test_least_item_cond_true(threshold, expected):
         [1, 2, 2, 3, 3, 1, 4], dims="lead", coords={"lead": np.arange(1, 1 + 7)}
     )
     cond = ds <= threshold
-    actual = last_item_cond_true(cond, "lead")
+    actual = _last_item_cond_true(cond, "lead")
     assert actual == expected
 
 
 def test_predictability_horizon_bootstrap_1d(perfectModelEnsemble_initialized_control):
     """test predictability_horizon for pm.bootstrap for 1d."""
     bskill = perfectModelEnsemble_initialized_control.bootstrap(
-        iterations=101,
+        iterations=201,
         metric="rmse",
         comparison="m2e",
         dim=["member", "init"],
@@ -27,13 +27,30 @@ def test_predictability_horizon_bootstrap_1d(perfectModelEnsemble_initialized_co
     )
     ph = predictability_horizon(bskill.sel(results="p", skill="uninitialized") <= 0.05)
     assert ph.tos.attrs["units"] == "years", print(ph.tos)
-    assert int(ph.tos) in [5, 6, 7]  # should be 6
+    assert int(ph.tos) in [5, 6, 7]  # should be 6, testing on the safe side
 
 
-def test_predictability_horizon_3d():
-    pass
+def test_predictability_horizon_3d(hindcast_recon_3d):
+    he = hindcast_recon_3d
+    # set one grid to all nan mimicking land
+    land = he._datasets["initialized"]["SST"][:, :, 0, 0]
+    he._datasets["initialized"]["SST"][:, :, 0, 0] = land.where(land == 100000)
+    # verify
+    skill = he.verify(
+        metric="acc",
+        comparison="e2o",
+        dim="init",
+        alignment="maximize",
+        reference=["persistence"],
+    )
+    from climpred.predictability_horizon import predictability_horizon
+
+    ph = predictability_horizon(
+        skill.sel(skill="initialized") > skill.sel(skill="persistence")
+    )
     # test all nan on land
-
-    # test never significant
-
-    # test always significant
+    print(ph)
+    assert ph["SST"][0, 0].isnull()
+    # test significant everywhere
+    assert (ph >= 1).all()
+    assert (ph.isel(nlat=-1) == 2).all()

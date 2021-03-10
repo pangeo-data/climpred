cfb3281569020c31e935e6f0df3a26b77f40a429
Aaron Spring <aaronspring@users.noreply.github.com>
2021-03-10 22:17:30 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,13 @@ extensions = [
     "IPython.sphinxext.ipython_console_highlighting",
 ]
 
+# Cupybutton configuration
+# See: https://sphinx-copybutton.readthedocs.io/en/latest/
+copybutton_prompt_text = r'>>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: '
+copybutton_prompt_is_regexp = True
+copybutton_only_copy_prompt_lines = True
+copybutton_remove_prompts = True
+
 extlinks = {
     "issue": ("https://github.com/pangeo-data/climpred/issues/%s", "GH#"),
     "pr": ("https://github.com/pangeo-data/climpred/pull/%s", "GH#"),

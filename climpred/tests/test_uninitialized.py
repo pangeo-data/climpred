f9333d8ed42d24f331ed052fafc285287ac36f0d
Aaron Spring <aaronspring@users.noreply.github.com>
2021-03-13 00:05:04 +0100

    Update test_uninitialized.py

--- climpred/tests/test_uninitialized.py
+++ climpred/tests/test_uninitialized.py
@@ -1,51 +1,8 @@
 import numpy as np
 import pytest
 
-from climpred.comparisons import HINDCAST_COMPARISONS
 from climpred.constants import VALID_ALIGNMENTS
-from climpred.metrics import DETERMINISTIC_HINDCAST_METRICS
 from climpred.reference import compute_uninitialized
-
-# uacc breaks
-DETERMINISTIC_HINDCAST_METRICS = DETERMINISTIC_HINDCAST_METRICS.copy()
-DETERMINISTIC_HINDCAST_METRICS.remove("uacc")
-
-
-@pytest.mark.parametrize("metric", DETERMINISTIC_HINDCAST_METRICS)
-@pytest.mark.parametrize("comparison", HINDCAST_COMPARISONS)
-def test_compute_uninitialized(
-    hind_ds_initialized_1d,
-    reconstruction_ds_1d,
-    hist_ds_uninitialized_1d,
-    metric,
-    comparison,
-):
-    """
-    Checks that compute uninitialized works without breaking.
-    """
-    category_edges = np.array([0.0, 0.5, 1.0])
-    if metric == "contingency":
-        metric_kwargs = {
-            "forecast_category_edges": category_edges,
-            "observation_category_edges": category_edges,
-            "score": "accuracy",
-        }
-    else:
-        metric_kwargs = {}
-    res = (
-        compute_uninitialized(
-            hind_ds_initialized_1d,
-            hist_ds_uninitialized_1d,
-            reconstruction_ds_1d,
-            metric=metric,
-            comparison=comparison,
-            **metric_kwargs
-        )
-        .isnull()
-        .any()
-    )
-    for var in res.data_vars:
-        assert not res[var]
 
 
 @pytest.mark.parametrize("alignment", VALID_ALIGNMENTS)

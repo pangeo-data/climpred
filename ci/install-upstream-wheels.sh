7463b16ebd5d6753d61741cd0b86ac0523c7ffbf
AS <aaron.spring@mpimet.mpg.de>
2021-04-11 17:40:29 +0200

    initial commit copied from xarray

---
+++ ci/install-upstream-wheels.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+conda uninstall -y --force \
+    numpy \
+    scipy \
+    pandas \
+    matplotlib \
+    dask \
+    cftime \
+    bottleneck \
+    xarray \
+    xskillscore \
+    climpred \
+    nc-time-axis \
+    netcdf4
+
+
+# to limit the runtime of Upstream CI
+python -m pip install pytest-timeout
+python -m pip install \
+    -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
+    --no-deps \
+    --pre \
+    --upgrade \
+    numpy \
+    scipy \
+    pandas
+python -m pip install \
+    -f https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com \
+    --no-deps \
+    --pre \
+    --upgrade \
+    matplotlib
+python -m pip install \
+    --no-deps \
+    --upgrade \
+    git+https://github.com/dask/dask \
+    git+https://github.com/Unidata/cftime \
+    git+https://github.com/pydata/bottleneck \
+    git+https://github.com/pydata/xarray  \
+    git+https://github.com/xarray-contrib/xskillscore \
+    git+https://github.com/SciTools/nc-time-axis \
+    git+https://github.com/Unidata/netcdf4-python

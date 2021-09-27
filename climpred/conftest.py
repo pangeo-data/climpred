a5495aa9f0fe2326f81c5463946405601767b71d
Aaron Spring <aaronspring@users.noreply.github.com>
2021-09-27 23:26:19 +0200

    Update conftest.py

--- climpred/conftest.py
+++ climpred/conftest.py
@@ -403,15 +403,15 @@ def hindcast_NMME_Nino34():
 
 
 @pytest.fixture()
- def da_lead():
-     """Small xr.DataArray with coords `init` and `lead`."""
-     lead = np.arange(5)
-     init = np.arange(5)
-     return xr.DataArray(
-         np.random.rand(len(lead), len(init)),
-         dims=["init", "lead"],
-         coords=[init, lead],
-     )
+def da_lead():
+    """Small xr.DataArray with coords `init` and `lead`."""
+    lead = np.arange(5)
+    init = np.arange(5)
+    return xr.DataArray(
+        np.random.rand(len(lead), len(init)),
+        dims=["init", "lead"],
+        coords=[init, lead],
+    )
 
 
 @pytest.fixture()

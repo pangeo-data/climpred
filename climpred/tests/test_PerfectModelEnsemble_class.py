817081935caeeeb0974c6f796621877f02ccedfd
Aaron Spring <aaronspring@users.noreply.github.com>
2021-11-26 23:08:57 +0100

    lint

--- climpred/tests/test_PerfectModelEnsemble_class.py
+++ climpred/tests/test_PerfectModelEnsemble_class.py
@@ -441,5 +441,6 @@ def testPerfectModelEnsemble_verify_groupby(
     )
     assert "month" in grouped_skill.dims
     grouped_skill = perfectModelEnsemble_initialized_control.verify(
-        **kw, groupby=perfectModelEnsemble_initialized_control.get_initialized().init.dt.month)
+        **kw,
+        groupby=perfectModelEnsemble_initialized_control.get_initialized().init.dt.month)
     assert "month" in grouped_skill.dims

6c7920d17c0b0355835f65c83cd961c8919b134f
AS <aaron.spring@mpimet.mpg.de>
2021-12-13 19:33:12 +0100

    climpred-docs in qs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -143,7 +143,7 @@ intersphinx_mapping = {
 # are breaking.
 # nbsphinx_allow_errors = True
 
-nbsphinx_kernel_name = "climpred-docs"
+nbsphinx_kernel_name = "climpred-docs-notebooks"
 
 
 # Napoleon configurations

32cc31b787a49dfdb11e540edaa33b4c76380db7
AS <aaron.spring@mpimet.mpg.de>
2022-02-02 15:30:06 +0100

    fix

--- climpred/classes.py
+++ climpred/classes.py
@@ -605,9 +605,7 @@ class PredictionEnsemble:
 
         return self._apply_func(sel_vars, varlist)
 
-    def __getattr__(
-        self, name: str
-    ):  # typing: ignore  # -> Callable[[VarArg(Any), KwArg(Any)], Any]
+    def __getattr__(self, name):  # -> Callable[[VarArg(Any), KwArg(Any)], Any]
         """Allow for ``xarray`` methods to be applied to our prediction objects.
 
         Args:

13213ced90d8fd9864259310d17ebd74d27d3082
AS <aaron.spring@mpimet.mpg.de>
2022-02-19 17:56:12 +0100

    rm print

--- climpred/bootstrap.py
+++ climpred/bootstrap.py
@@ -484,7 +484,6 @@ def resample_skill_loop(self, iterations, resample_dim, verify_kwargs):
                     "uninitialized"
                 ] = self.generate_uninitialized().get_uninitialized()
             else:
-                print(self.get_uninitialized())
                 self_for_loop._datasets["uninitialized"] = _resample(
                     self.get_uninitialized(), "member"
                 )

680f8d10ed16a71fd28bc7dd6bf17d76a513cbfe
AS <aaron.spring@mpimet.mpg.de>
2022-03-17 22:39:05 +0100

    fix

--- climpred/logging.py
+++ climpred/logging.py
@@ -20,20 +20,21 @@ def log_hindcast_verify_inits_and_verifs(
     """At each lead, log the inits and verification dates being used in computations."""
     if reference is None:
         reference = "initialized"
-    logging.info(
-        f"{reference} | lead: {str(lead).zfill(2)} | "
-        # This is the init-sliced forecast, thus displaying actual
-        # initializations.
-        f"inits: {inits[lead].min().values}"
-        f"-{inits[lead].max().values} | "
-        # This is the verification window, thus displaying the
-        # verification dates.
-        f"verifs: {verif_dates[lead].min()}"
-        f"-{verif_dates[lead].max()}"
-    )
     time_datetimeindex = (
         True if not isinstance(verif_dates[lead], xr.CFTimeIndex) else False
     )
+    if len(inits[lead]) > 1 or not time_datetimeindex:
+        logging.info(
+            f"{reference} | lead: {str(lead).zfill(2)} | "
+            # This is the init-sliced forecast, thus displaying actual
+            # initializations.
+            f"inits: {inits[lead].min().values}"
+            f"-{inits[lead].max().values} | "
+            # This is the verification window, thus displaying the
+            # verification dates.
+            f"verifs: {verif_dates[lead].min()}"
+            f"-{verif_dates[lead].max()}"
+        )
     init_output_iter = (
         inits[lead].to_index() if time_datetimeindex else inits[lead].values
     )

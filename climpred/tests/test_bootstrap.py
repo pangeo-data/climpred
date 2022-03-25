7104b2207a7cfd1c54d1980ac0867c2fa6963ebb
Aaron Spring <aaronspring@users.noreply.github.com>
2022-03-25 21:23:37 +0100

    Update test_bootstrap.py

--- climpred/tests/test_bootstrap.py
+++ climpred/tests/test_bootstrap.py
@@ -517,7 +517,7 @@ def test_generate_uninitialized(hindcast_hist_obs_1d):
     """Test HindcastEnsemble.generate_uninitialized()"""
     from climpred.stats import rm_poly
 
-    hindcast_hist_obs_1d_new = hindcast_hist_obs_1d.map(
+    hindcast_hist_obs_1d_new = hindcast_hist_obs_1d.copy().map(
         rm_poly, dim="init_or_time", deg=2
     ).generate_uninitialized()
     # new created

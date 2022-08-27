726cfc5dcc29c75a5b8d0d7befac554b425b5c8a
Aaron Spring <aaronspring@users.noreply.github.com>
2022-08-27 19:11:28 +0200

    Apply suggestions from code review

--- climpred/tests/test_bias_removal.py
+++ climpred/tests/test_bias_removal.py
@@ -551,6 +551,7 @@ def test_remove_bias_dont_drop(hindcast_NMME_Nino34):
         how="additive_mean", alignment="same_verifs"
     )
     assert "lead" in detrended.get_initialized().dims
+    print(detrended.get_initialized())
     detrended.verify(
         metric="rmse", comparison="e2o", dim="init", alignment="same_inits"
     )

8ea6cb4339790f0006046784c4fc778627da7767
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-10-03 12:16:48 -0400

    sort package metadata, add Python3.11

--- setup.py
+++ setup.py
@@ -13,16 +13,17 @@ with open("requirements.txt") as f:
 
 CLASSIFIERS = [
     "Development Status :: 5 - Production/Stable",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Intended Audience :: Science/Research",
     "Intended Audience :: Education",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
     "Natural Language :: English",
     "Operating System :: OS Independent",
+    "Operating System :: OS Independent",
     "Programming Language :: Python :: 3 :: Only",
-    "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Topic :: Scientific/Engineering :: Atmospheric Science",
 ]
 

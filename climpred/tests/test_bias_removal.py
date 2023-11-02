6ba99dbf2f9f1b00dd6fa9d7f10ccadc3683946a
Aaron Spring <12237157+aaronspring@users.noreply.github.com>
2023-11-02 19:55:22 +0100

    Update test_bias_removal.py

--- climpred/tests/test_bias_removal.py
+++ climpred/tests/test_bias_removal.py
@@ -26,7 +26,7 @@ XCLIM_BIAS_CORRECTION_METHODS = copy.copy(XCLIM_BIAS_CORRECTION_METHODS)
 XCLIM_BIAS_CORRECTION_METHODS.remove("LOCI")
 XCLIM_BIAS_CORRECTION_METHODS.remove("PrincipalComponents")
 # XCLIM_BIAS_CORRECTION_METHODS.remove("DetrendedQuantileMapping")
-XCLIM_BIAS_CORRECTION_METHODS.remove("QuantileDeltaMapping")
+# XCLIM_BIAS_CORRECTION_METHODS.remove("QuantileDeltaMapping")
 
 
 def _adjust_metric_kwargs(metric_kwargs=None, how=None, he=None):

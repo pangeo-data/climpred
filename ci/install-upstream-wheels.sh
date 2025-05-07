a914076146c2da7b95a543d290b1f7b7eea516a1
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2025-05-07 15:38:24 -0400

    adjust upstream CI config

--- ci/install-upstream-wheels.sh
+++ ci/install-upstream-wheels.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
 conda uninstall -y --force \
+    bias_correction \
+    cftime \
+    dask \
+    matplotlib \
+    nc-time-axis \
     numpy \
     pandas \
-    matplotlib \
-    dask \
-    cftime \
-    nc-time-axis \
-    # bottleneck \
     xarray \
-    xskillscore \
     xclim \
-    bias_correction \
+    xskillscore \
     climpred
 
-
 # to limit the runtime of Upstream CI
-python -m pip install pytest-timeout
 python -m pip install \
     -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
     --no-deps \
@@ -25,10 +22,10 @@ python -m pip install \
     numpy \
     pandas
 python -m pip install \
-    -f https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com \
-    --no-deps \
-    --pre \
     --upgrade \
+    --pre \
+    --index-url https://pypi.anaconda.org/scientific-python-nightly-wheels/simple \
+    --extra-index-url https://pypi.org/simple \
     matplotlib
 python -m pip install \
     --no-deps \
@@ -37,7 +34,6 @@ python -m pip install \
     git+https://github.com/Unidata/cftime \
     git+https://github.com/SciTools/nc-time-axis \
     git+https://github.com/pydata/xarray  \
-    # git+https://github.com/pydata/bottleneck  \
     git+https://github.com/xarray-contrib/xskillscore \
     git+https://github.com/xgcm/xrft \
     git+https://github.com/pankajkarman/bias_correction
